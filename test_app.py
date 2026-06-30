e8069ab50dd0f2b2c831162f6139df288ed2078a
wanghsingchuan <allster134679@gmail.com>
2026-06-30 21:58:39 +0800

    添加程式檢查

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import pytest
+from app import app
+ 
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+ 
+        yield client
+
+def test_hello(client):
+ 
+    response = client.get("/")
+    assert response.status_code == 200
+
+ 
+def test_health(client):
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.get_json()["status"] == "healthy"
